454f953aca9e3f00d9584665ee18251eec7f13dc
Kok Yee Yang <yee_yang94@hotmail.com>
2019-05-08 19:21:58 +0800

    HR-44 - First template for add candidate form

---
+++ protected/views/registration/candidateForm.php
@@ -0,0 +1,12 @@
+<form action="" style="width:50%">
+  <fieldset>
+  	<div class="row">
+  		<div class="common_content_inner_wrapper">
+		    <header>APPLCATION FOR EMPLOYMENT</header>
+			    Position applied for:<input type="text" name="firstname">
+			</div>
+	    <input type="file" id="pictureBox" onchange="readUrl(picture)" title="Please choose a picture">
+		</div>
+    <input type="submit" value="Submit">
+  </fieldset>
+</form>
