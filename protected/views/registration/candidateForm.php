f91bbb4f5da658d9a4200a98b5a09f39a4d8a529
Kok Yee Yang <yee_yang94@hotmail.com>
2019-05-09 15:27:22 +0800

    HR-44 - Created separate candidateForm css file

--- protected/views/registration/candidateForm.php
+++ protected/views/registration/candidateForm.php
@@ -1,21 +1,25 @@
-<form action="" style="width:50%">
-	<div class="row">
-		<!-- <div class="common_content_inner_wrapper"> -->
-		<div class="tpl_main_wrapper .common_form">
-			<div class="row">
-				<div id="candidateFormTitle">
-			    <div id="candidateFormHeader">
-			    	<img src="<?php echo HTTP_MEDIA_IMAGES . '/alllanguages/sagaos_logo.png?sv='.SITE_VERSION;?>" alt="" />
-			    	<?php echo Yii::t('app', 'APPLICATION FOR EMPLOYMENT'); ?>
-		    	</div>
-			    <div id="positionInput">
-				    <?php echo Yii::t('app', 'Position applied for'); ?>:
-				    <input type="text" name="firstname" class="inputLine">
-				  </div>
-			  </div>
-		    <input type="file" id="pictureBox" onchange="readUrl(picture)" title="Please choose a picture">
+<link href="candidateForm.css" rel="stylesheet" type="text/css" media="all">
+<form action="" id="candidateForm">
+	<!-- <div class="row"> -->
+		<div id="candidateFormTitle">
+	    <div id="candidateFormHeader">
+	    	<div class="item_row animate0 bounceIn logo">
+		    	<img src="<?php echo HTTP_MEDIA_IMAGES . '/alllanguages/sagaos_logo.png?sv='.SITE_VERSION;?>" alt="" />
+		    </div>
+	    	<?php echo Yii::t('app', 'APPLICATION FOR EMPLOYMENT'); ?>
+    	</div>
+	    <div id="positionInput">
+		    <?php echo Yii::t('app', 'Position applied for'); ?>:
+		    <input type="text" name="firstname" class="inputLine">
 		  </div>
-		</div>
-    <input type="submit" value="Submit">
-	</div>
+	  </div>
+    <input type="file" id="pictureBox" onchange="readUrl(picture)" title="Please choose a picture">
+  <!-- </div> -->
+  <!-- primaryInfoBox is for each of the big boxes to input information -->
+  <!-- <div class="row"> -->
+	  <div class="primaryInfoBox">
+	  	hellllooooo
+	  </div>
+	<!-- </div> -->
+  <input type="submit" value="Submit">
 </form>
