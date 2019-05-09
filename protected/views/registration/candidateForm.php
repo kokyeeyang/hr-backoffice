8083bf80abe9f2dc25607673ab70f8dbb2ecd054
Kok Yee Yang <yee_yang94@hotmail.com>
2019-05-09 12:00:09 +0800

    HR-44 - Streamlined css property and added sagaos logo

--- protected/views/registration/candidateForm.php
+++ protected/views/registration/candidateForm.php
@@ -1,12 +1,21 @@
 <form action="" style="width:50%">
-  <fieldset>
-  	<div class="row">
-  		<div class="common_content_inner_wrapper">
-		    <header>APPLCATION FOR EMPLOYMENT</header>
-			    Position applied for:<input type="text" name="firstname">
-			</div>
-	    <input type="file" id="pictureBox" onchange="readUrl(picture)" title="Please choose a picture">
+	<div class="row">
+		<!-- <div class="common_content_inner_wrapper"> -->
+		<div class="tpl_main_wrapper .common_form">
+			<div class="row">
+				<div id="candidateFormTitle">
+			    <div id="candidateFormHeader">
+			    	<img src="<?php echo HTTP_MEDIA_IMAGES . '/alllanguages/sagaos_logo.png?sv='.SITE_VERSION;?>" alt="" />
+			    	<?php echo Yii::t('app', 'APPLICATION FOR EMPLOYMENT'); ?>
+		    	</div>
+			    <div id="positionInput">
+				    <?php echo Yii::t('app', 'Position applied for'); ?>:
+				    <input type="text" name="firstname" class="inputLine">
+				  </div>
+			  </div>
+		    <input type="file" id="pictureBox" onchange="readUrl(picture)" title="Please choose a picture">
+		  </div>
 		</div>
     <input type="submit" value="Submit">
-  </fieldset>
+	</div>
 </form>
