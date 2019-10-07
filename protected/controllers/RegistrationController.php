ae06760feb8b5cc1652705cc00f04aecd7a58b64
Kok Yee Yang <yee_yang94@hotmail.com>
2019-10-07 20:25:49 +0800

    Got rid of unnecessary code

--- protected/controllers/RegistrationController.php
+++ protected/controllers/RegistrationController.php
@@ -814,42 +814,6 @@ class RegistrationController extends Controller
 		$pdf->SetFont('helvetica', '', 9);
 		$pdf->AddPage();
 
-    $html = '<h1>HTML Example</h1>
-Some special characters: &lt; € &euro; &#8364; &amp; è &egrave; &copy; &gt; \\slash \\\\double-slash \\\\\\triple-slash
-<h2>List</h2>
-List example:
-<ol>
-    <li><b>bold text</b></li>
-    <li><i>italic text</i></li>
-    <li><u>underlined text</u></li>
-    <li><b>b<i>bi<u>biu</u>bi</i>b</b></li>
-    <li><a href="http://www.tecnick.com" dir="ltr">link to http://www.tecnick.com</a></li>
-    <li>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.<br />Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</li>
-    <p>TESTING LAAAAA </p>
-    <li>SUBLIST
-        <ol>
-            <li>row one
-                <ul>
-                    <li>sublist</li>
-                </ul>
-            </li>
-            <li>row two</li>
-        </ol>
-    </li>
-    <li><b>T</b>E<i>S</i><u>T</u> <del>line through</del></li>
-    <li><font size="+3">font + 3</font></li>
-    <li><small>small text</small> normal <small>small text</small> normal <sub>subscript</sub> normal <sup>superscript</sup> normal</li>
-</ol>
-<dl>
-    <dt>Coffee</dt>
-    <dd>Black hot drink</dd>
-    <dt>Milk</dt>
-    <dd>White cold drink</dd>
-</dl>
-</div>';
-
-		$html2= '<p>asaddas</p> <p>dasfds</p> <p>fds</p> <p>fdssdf</p> <p>sdf</p> <p>qweqeqwewqe</p> <p>ewqeqweqqwe</p> <p>sdfsassasSDSADSADSAD</p> <p>asassawqwe</p> <p>sdf</p> <p>qwreewqrwe</p> <p>FOR HR MANAGER LA</p>';
-
 		$pdf->writeHTML($sanitizedOfferLetterTemplate, true, false, true, false);
 		$pdf->lastPage();
 
@@ -887,13 +851,17 @@ List example:
 		      header("HTTP/1.1 400 Not an Image");
 		      return;
 		  }
+
+		  // var_dump($_FILES);exit;
 		  
-		  $fileName = "webroot/themes/uploads/" . $temp['name'];
-		  move_uploaded_file($temp['tmp_name'], $fileName);
+		  // $fileName = "uploads/" . $temp['name'];
+		  $fileName = $_SERVER['DOCUMENT_ROOT'] . "themes/uploads/" . $temp['name'];
+		  // move_uploaded_file($temp['tmp_name'], $fileName);
+		  move_uploaded_file($_FILES["file"]["tmp_name"], getcwd() . "/themes/uploads/" . $_FILES["file"]["name"]);
 		  
 		  // Return JSON response with the uploaded file path.
 		  echo json_encode(array(
-		      'file_path' => $fileName
+	      'file_path' => $fileName
 		  ));
 		}
 	}
