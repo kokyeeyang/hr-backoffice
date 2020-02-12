6a0dfbbe162c9dee55565c6a115d2a6aa0ff7d4e
Kok Yee Yang <yee_yang94@hotmail.com>
2020-02-12 11:18:55 +0800

    HR-55 - Added css for add new department page

--- protected/views/admin/departmentDetails.php
+++ protected/views/admin/departmentDetails.php
@@ -6,7 +6,7 @@
     <div class="breadcrumb-bottom breadcrumb-bottom-chart">
       <div class="title">
         <span>
-          <?php echo $header; ?>
+	  <?php echo $header; ?>
         </span>
       </div>
     </div>
@@ -19,26 +19,33 @@
     </h4>
     <form method="post" enctype="multipart/form-data" id="departmentForm" name="departmentForm" action="<?php echo $formAction; ?>" >
       <table style="line-height: 32px;padding-left: 10px;font-size: 15px;">
-        <tr>
-          <td><?php echo Yii::t('app', 'Please specify your department name'); ?> </td>
-          <td>:</td>
-          <td><input type="text" name="newDepartment" id="newDepartment" value="<?php echo $departmentTitle; ?>" required/>
-        </tr>
-        <tr>
-          <td>
-            <?php echo Yii::t('app', 'Give a short description'); ?>
-          </td>
-          <td>:</td>
-          <td>
-            <textarea rows="4" name="departmentDescription" id="departmentDescription" cols="22" required/><?php echo $departmentDescription; ?></textarea>
-          </td>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <input type="submit" value="<?php echo $buttonTitle; ?>">
-          </td>
-        </tr>
+	<div class="grid_block">
+	  <div class="lable_block">
+	    <div class="lables">
+	      <span><?php echo Yii::t('app', 'Please specify your department name'); ?> </span>
+	      <span>:</span>
+	    </div>
+	    <div class="lables2">
+	      <span><input type="text" name="newDepartment" id="newDepartment" value="<?php echo $departmentTitle; ?>" required/></span>
+	    </div>
+	  </div>
+	  <div class="lable_block">
+	    <div class="lables">
+	      <span>
+		<?php echo Yii::t('app', 'Give a short description'); ?>
+	      </span>
+	      <span>:</span>
+	    </div>
+	    <div class="lables2">;
+	      <span>
+		<textarea rows="4" name="departmentDescription" id="departmentDescription" cols="22" required/><?php echo $departmentDescription; ?></textarea>
+	      </span>
+	    </div>
+	  </div>
+	  <div class="lable_block">
+	      <input type="submit" value="<?php echo $buttonTitle; ?>">
+	  </div>
+	</div>
       </table>
     </form>
   </div>
