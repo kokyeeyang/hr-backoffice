a5d5bd2c2cb371c3e0c5e36b54e5bb4eb4a5a102
Kok Yee Yang <yee_yang94@hotmail.com>
2020-02-17 16:52:53 +0800

    HR-55 - Added more css for input pages

--- protected/views/onboarding/onboardingChecklistTemplateDetails.php
+++ protected/views/onboarding/onboardingChecklistTemplateDetails.php
@@ -155,16 +155,15 @@
 	      </tr>
 	    </tbody>
 	  </table>
+	  <!-- need to add a bar containing title, description, department owner, is_offboarding_item, status, is_managerial for onboarding items -->
+	  <!-- would need ajax to append a dropdown to add onboarding items -->
+	  <!-- would need to pass php array containing all the onboarding items that are available in the database into a dropdown menu for users to choose-->
+	  <!-- ajax would then populate the data for the onboarding item that the user chose -->
+	  <button type="button" id="appendOnboardingItem" title="Add more onboarding items to this template">+</button>
+	  <br/><br/>
+	  <button title="<?php echo $buttonTitle; ?>" class="<?php echo $buttonClass; ?>" disabled><?php echo $buttonShortTitle; ?></button>
 	</fieldset>
-      <!--<input type="button" id="addOnboardingItem" title="Add a new onboarding item for this template" value="Add new item"/>-->
-	<!-- need to add a bar containing title, description, department owner, is_offboarding_item, status, is_managerial for onboarding items -->
-	<!-- would need ajax to append a dropdown to add onboarding items -->
-	<!-- would need to pass php array containing all the onboarding items that are available in the database into a dropdown menu for users to choose-->
-	<!-- ajax would then populate the data for the onboarding item that the user chose -->
       </div>
-      <button type="button" id="appendOnboardingItem" title="Add more onboarding items to this template">+</button>
-      <br/><br/>
-      <button title="<?php echo $buttonTitle; ?>" class="<?php echo $buttonClass; ?>" disabled><?php echo $buttonShortTitle; ?></button>
     </form>
   </div>
 </div>
