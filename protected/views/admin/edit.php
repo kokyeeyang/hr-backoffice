545860f5b1877de85b0bddfba52c085814039ec9
Kok Yee Yang <yeeyang.kok@sagaos.com>
2020-03-17 12:43:55 +0800

    Deleted unused css file

--- protected/views/admin/edit.php
+++ protected/views/admin/edit.php
@@ -1,63 +1,21 @@
-<style>
-/*  body {font-family: Arial;}
-
-   Style the tab 
-  .tab {
-    overflow: hidden;
-    border: 1px solid #ccc;
-    background-color: #f1f1f1;
-  }
-
-   Style the buttons inside the tab 
-  .tab button {
-    background-color: inherit;
-    float: left;
-    border: none;
-    outline: none;
-    cursor: pointer;
-    padding: 14px 16px;
-    transition: 0.3s;
-    font-size: 17px;
-  }
-
-   Change background color of buttons on hover 
-  .tab button:hover {
-    background-color: #ddd;
-  }
-
-   Create an active/current tablink class 
-  .tab button.active {
-    background-color: #ccc;
-  }
-
-   Style the tab content 
-  .tabcontent {
-    display: none;
-    padding: 6px 12px;
-    border: 1px solid #ccc;
-    border-top: none;
-  }/**/
-
-</style>
 
 <script>
-    function openTab(evt, cityName) {
-      var i, tabcontent, tablinks;
-      tabcontent = document.getElementsByClassName("tabcontent");
-      for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-      }
-      tablinks = document.getElementsByClassName("tablinks");
-      for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-      }
-      document.getElementById(cityName).style.display = "block";
-      evt.currentTarget.className += " active";
-    }
+//    function openTab(evt, cityName) {
+//      var i, tabcontent, tablinks;
+//      tabcontent = document.getElementsByClassName("tabcontent");
+//      for (i = 0; i < tabcontent.length; i++) {
+//        tabcontent[i].style.display = "none";
+//      }
+//      tablinks = document.getElementsByClassName("tablinks");
+//      for (i = 0; i < tablinks.length; i++) {
+//        tablinks[i].className = tablinks[i].className.replace(" active", "");
+//      }
+//      document.getElementById(cityName).style.display = "block";
+//      evt.currentTarget.className += " active";
+//    }
 </script>
 
 <script src="admin-edit.js"></script>
-<style src="admin-edit.css"></style>
 
 <div class="breadcrumb">
   <div class="breadcrumb_wrapper">
